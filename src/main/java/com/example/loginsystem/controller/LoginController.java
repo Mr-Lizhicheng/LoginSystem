aba5a2734498a0033e1ed9d9a45118dbf1e49d64
Mr-Lizhicheng <3254162498@qq.com>
2024-06-24 09:51:58 +0800

    添加登录测试接口

--- src/main/java/com/example/loginsystem/controller/LoginController.java
+++ src/main/java/com/example/loginsystem/controller/LoginController.java
@@ -1,9 +1,16 @@
 package com.example.loginsystem.controller;
 
+import com.example.loginsystem.entity.User;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.http.HttpCookie;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.net.http.HttpResponse;
 
 @Controller
 public class LoginController {
@@ -14,11 +21,22 @@ public class LoginController {
         return "testGet Successfully";
     }
 
-
     @PostMapping("/testPost")
     @ResponseBody
     public String testPost() {
         return "testPost Successfully";
+    }
+
+    @PostMapping("/login")
+    @ResponseBody
+    public String login(User user, HttpSession session, HttpServletResponse response) {
+        if (user.getName().equals("黎志城") && user.getPassword().equals("123456")) {
+            user.setTicket("1");
+            session.setAttribute("user",user);
+            response.addCookie(new Cookie("ticket",user.getTicket()));
+            return "登录成功！";
+        }
+        return "登录失败！";
     }
 
 }
